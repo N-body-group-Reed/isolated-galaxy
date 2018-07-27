f7057bbc9041520046b36dc7c6c9f65d0d76674b
Miles Cohen <miles.cohen15@gmail.com>
2018-07-27 03:00:33 -0700

    Added 4 different density profiles everywhere.
    NFW, Jaffe, Hernquist, and arbitrary density profile created.  Simulation runs for each one, as reflected in the makefile.  All have identical .param files discounting name changes.

--- code/eqHaloDef/particleCount/init_cond.py
+++ code/eqHaloDef/particleCount/init_cond.py
@@ -6,6 +6,21 @@ from pyICs.am_profiles import *
 from pyICs.equilibrium_halos import *
 from pyICs.tools import *
 
-sim = EquilibriumHalo(fname = "halo2.out", n_particles = 1e7)
-sim.make_halo()
-sim.finalize()
+parsNFW    = {'alpha': 1, 'beta': 3, 'gamma': 1, 'c': 20}
+parsHrn    = {'alpha': 1, 'beta': 4, 'gamma': 1, 'c': 1}
+parsJff    = {'alpha': 1, 'beta': 4, 'gamma': 2, 'c': 1}
+parsArb    = {'alpha': 1.4, 'beta': 4, 'gamma': 2, 'c': 1}
+simNFW = EquilibriumHalo(fname = "haloNFW.out", n_particles = 1e7, pars=parsNFW)
+simHrn = EquilibriumHalo(fname = "haloHrn.out", n_particles = 1e7, pars=parsHrn)
+simJff = EquilibriumHalo(fname = "haloJff.out", n_particles = 1e7, pars=parsHrn)
+simArb = EquilibriumHalo(fname = "haloArb.out", n_particles = 1e7, pars=parsArb)
+
+simNFW.make_halo()
+simHrn.make_halo()
+simJff.make_halo()
+simArb.make_halo()
+
+simNFW.finalize()
+simHrn.finalize()
+simJff.finalize()
+simArb.finalize()
