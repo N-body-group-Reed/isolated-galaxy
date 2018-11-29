2b1aae53d09c9fd5f10b50a75f26240a50f80a28
johnny powell <dna@reed.edu>
2018-11-29 14:54:10 -0800

    Update init_cond.py

--- code/eqHaloDef/init_cond.py
+++ code/eqHaloDef/init_cond.py
@@ -1,5 +1,8 @@
 # Initial conditions to create simplest possible halo initial conditoins
-# This will not form stars.  Only a dark matter and gas halo
+# This will not form stars.  Only a dark matter and gas halo.
+# A reference for why this code will not form stars would be helpful
+# presumably the pyICs site has a clue
+#
 from pyICs.density_profiles import *
 from pyICs.am_profiles import *
 from pyICs.equilibrium_halos import *
